3214f0afb2fc042333f8e0721645db51c6240773
野瀬田　裕樹 <noseda-yuki@dmm.com>
2024-07-09 19:50:47 +0900

    SwiftUIからstoryboardでの作成に切り替え

---
+++ UpcomingFeatureFlagsSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UpcomingFeatureFlagsSample
+//
+//  Created by 野瀬田 裕樹 on 2024/07/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
