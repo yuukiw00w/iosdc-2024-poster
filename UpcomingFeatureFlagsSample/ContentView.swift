dea37dc470fb11467722a15038dde7cd029f6fe4
野瀬田　裕樹 <noseda-yuki@dmm.com>
2024-07-09 20:09:38 +0900

    ExistentialAnyで警告が出る状態にする

--- UpcomingFeatureFlagsSample/ContentView.swift
+++ UpcomingFeatureFlagsSample/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button("ExistentialAnySample") {
+                let existentialAny = ExistentialAnySample()
+                doPrintSample(existentialAny: existentialAny)
+            }
         }
         .padding()
     }
---
+++ UpcomingFeatureFlagsSample/ExistentialAnySample.swift
@@ -0,0 +1,22 @@
+//
+//  ExistentialAnySample.swift
+//  UpcomingFeatureFlagsSample
+//
+//  Created by 野瀬田 裕樹 on 2024/07/09.
+//
+
+import Foundation
+
+class ExistentialAnySample: ExistentialAnySampleProtocol {
+    func printSample() {
+        print("sample")
+    }
+}
+
+protocol ExistentialAnySampleProtocol {
+    func printSample()
+}
+
+func doPrintSample(existentialAny: ExistentialAnySampleProtocol) {
+    existentialAny.printSample()
+}
