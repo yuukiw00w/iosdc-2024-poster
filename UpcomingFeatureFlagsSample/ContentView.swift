dde361499edfda56c3b03fde01179891914fc9f1
野瀬田　裕樹 <noseda-yuki@dmm.com>
2024-07-09 13:47:26 +0900

    gitignoreの追加とxcodeでの新規プロジェクト作成

---
+++ UpcomingFeatureFlagsSample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UpcomingFeatureFlagsSample
+//
+//  Created by 野瀬田 裕樹 on 2024/07/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UpcomingFeatureFlagsSample/UpcomingFeatureFlagsSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  UpcomingFeatureFlagsSampleApp.swift
+//  UpcomingFeatureFlagsSample
+//
+//  Created by 野瀬田 裕樹 on 2024/07/09.
+//
+
+import SwiftUI
+
+@main
+struct UpcomingFeatureFlagsSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
