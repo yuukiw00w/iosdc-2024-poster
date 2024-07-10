bc318266e4b702bcb50f6a29f82271de5105078d
野瀬田　裕樹 <noseda-yuki@dmm.com>
2024-07-10 15:03:12 +0900

    BareSlashRegexLiterals対応 (#14)
    #6
    -enable-upcoming-feature BareSlashRegexLiterals を付与する以外にも、 Enable Bare Slash Regex Literals をYESに設定することでも利用可能
    NSRegularExpressionを使った従来の書き方ではなく、 /My name is (.*)/ のように/で囲って正規表現リテラルを書くことができる

--- UpcomingFeatureFlagsSample/RegexLiteralsSample.swift
+++ UpcomingFeatureFlagsSample/RegexLiteralsSample.swift
@@ -9,19 +9,9 @@ import Foundation
 
 func printRegexLiteralMatch() {
     let text = "My name is Noseda"
-    let pattern = "My name is (.*)"
+    let regex = /My name is (.*)/
 
-    do {
-        let regex = try NSRegularExpression(pattern: pattern)
-        let nsRange = NSRange(text.startIndex..<text.endIndex, in: text)
-        
-        if let match = regex.firstMatch(in: text, options: [], range: nsRange) {
-            if let range = Range(match.range(at: 1), in: text) {
-                let matchedString = String(text[range])
-                print(matchedString)
-            }
-        }
-    } catch {
-        print("Invalid regex pattern")
+    if let match = text.matches(of: regex).first {
+        print(match.1)
     }
 }
