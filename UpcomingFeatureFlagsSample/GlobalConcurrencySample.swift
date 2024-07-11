5b64a93ce010092d6fa732d3b8c8fa1fed279526
野瀬田　裕樹 <noseda-yuki@dmm.com>
2024-07-11 18:45:30 +0900

    GlobalConcurrencyで警告が出る状態にする

---
+++ UpcomingFeatureFlagsSample/GlobalConcurrencySample.swift
@@ -0,0 +1,12 @@
+//
+//  GlobalConcurrencySample.swift
+//  UpcomingFeatureFlagsSample
+//
+//  Created by 野瀬田 裕樹 on 2024/07/11.
+//
+
+import Foundation
+
+final class GlobalConcurrencySample {}
+
+var sampleGlobalVar: GlobalConcurrencySample = .init()
