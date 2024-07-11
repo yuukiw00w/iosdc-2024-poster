2b2893cdca447f9ed8e3f2972e4cc239cc356495
野瀬田　裕樹 <noseda-yuki@dmm.com>
2024-07-11 17:54:15 +0900

    DisableOutwardActorInferenceの事前対応

---
+++ UpcomingFeatureFlagsSample/DisableOutwardActorInferenceSample.swift
@@ -0,0 +1,35 @@
+//
+//  DisableOutwardActorInferenceSample.swift
+//  UpcomingFeatureFlagsSample
+//
+//  Created by 野瀬田 裕樹 on 2024/07/11.
+//
+
+import Foundation
+
+@propertyWrapper
+struct MainActorWrapper<Wrapped> {
+    @MainActor var wrappedValue: Wrapped
+}
+
+struct MainActorIsolationInferenceSample {
+    @MainActorWrapper var intValue: Int = 0
+    nonisolated func nonisolatedFunc() {}
+    func isolatedFunc() {}
+}
+
+actor SampleActor {
+    let mainActorSample: MainActorIsolationInferenceSample
+    
+    init(mainActorSample: MainActorIsolationInferenceSample) {
+        self.mainActorSample = mainActorSample
+    }
+    
+    func nonisolatedFunc() {
+        mainActorSample.nonisolatedFunc()
+    }
+    
+    func isolatedFunc() async {
+        await mainActorSample.isolatedFunc()
+    }
+}
